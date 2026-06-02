bbb2a0cb4bb8b815224cd5f95fd9ff37507cc9d4
sasiis80-gif <sasini.somaratne80@gmail.com>
2026-06-02 19:51:45 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+#Parent class
+class Person(object):
+    def __init__(self, name, idnumber):
+        self.name = name
+        self.idnumber = idnumber
+    def display(self):
+        print(self.name)
+        print(self.idnumber)
+
+#Child class
+class Employee(Person):
+    def __init__(self, name, idnumber, salary, post):
+        self.salary = salary
+        self.post = post
+        Person.__init__(self, name, idnumber)
+a = Employee("Asanka", 1388, 28396, "Intern")
+a.display()
